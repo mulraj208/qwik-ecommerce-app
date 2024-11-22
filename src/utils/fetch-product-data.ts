d9d1847019b8948a9d460b1bf4ee8d9b757b74f4
Mulraj Gupta <mulrajgupta208@gmail.com>
2024-11-22 18:59:36 +0530

    Stable branch Initial Commit

--- src/utils/fetch-product-data.ts
+++ src/utils/fetch-product-data.ts
@@ -4,6 +4,7 @@ import {server$} from "@builder.io/qwik-city";
 export const fetchProductData = server$(async (productId: string) => {
     try {
         const {shopperProducts} = await getApiClients();
+        console.log('shopperProducts');
 
         return await shopperProducts.getProduct({
             parameters: {
