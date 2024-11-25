1314354bac4872e9f4df3d854afeb030c8edca67
Mulraj Gupta <mulrajgupta208@gmail.com>
2024-11-25 13:10:00 +0530

    Refactoring

--- src/components/ecommerce/category-menu/category-menu.tsx
+++ src/components/ecommerce/category-menu/category-menu.tsx
@@ -1,20 +1,37 @@
-import {component$} from "@builder.io/qwik";
+import {component$, Resource, useResource$} from "@builder.io/qwik";
 import CategoryMenuPopover from "~/components/ecommerce/category-menu/category-menu-popover";
+import config from "~/config/dw";
+import {CAT_MENU_DEFAULT_NAV_SSR_DEPTH, CAT_MENU_DEFAULT_ROOT_CATEGORY} from "~/constants";
+import type {levelZeroCategoriesQuery} from "~/components/starter/header/header";
 
-interface CategoryMenuProps {
-  categories: Array<CommerceSDK.Category>
-}
+interface CategoryMenuProps {}
 
-export default component$<CategoryMenuProps>(({ categories }) => {
+export default component$<CategoryMenuProps>(() => {
+    const apiResource = useResource$(async () => {
+        await (new Promise((res) => setTimeout(() => res(''), 3000)));
+
+        const response = await fetch(`${config.PUBLIC_API_ORIGIN}/api/commerce-sdk-react/category?id=${CAT_MENU_DEFAULT_ROOT_CATEGORY}&levels=${CAT_MENU_DEFAULT_NAV_SSR_DEPTH}`);
+        const data = await response.json();
+
+        return data.data as unknown as levelZeroCategoriesQuery;
+    });
+
   return (
-      <div class="flex-1">
-        <nav>
-          <div class="flex min-w-[20rem] pl-4 items-start">
-            <ul class="flex flex-wrap items-center">
-              {categories.map((category) => <CategoryMenuPopover key={category.name} category={category} />)}
-            </ul>
-          </div>
-        </nav>
-      </div>
+      <Resource
+          value={apiResource}
+          onPending={() => <p>Loading...</p>}
+          onResolved={({categories}) => (
+              <div class="flex-1">
+                  <nav>
+                      <div class="flex min-w-[20rem] pl-4 items-start">
+                          <ul class="flex flex-wrap items-center">
+                              {categories.map((category) => <CategoryMenuPopover key={category.name} category={category}/>)}
+                          </ul>
+                      </div>
+                  </nav>
+              </div>
+          )}
+          onRejected={(error) => <p>Error: {error.message}</p>}
+      />
   );
 });
--- src/components/starter/header/header.tsx
+++ src/components/starter/header/header.tsx
@@ -1,43 +1,27 @@
-import {component$, Resource, useResource$} from "@builder.io/qwik";
+import {component$} from "@builder.io/qwik";
 import {Link} from "@builder.io/qwik-city";
 import { LuMenu } from "@qwikest/icons/lucide";
-import {CAT_MENU_DEFAULT_NAV_SSR_DEPTH, CAT_MENU_DEFAULT_ROOT_CATEGORY} from "~/constants";
 import CategoryMenu from "~/components/ecommerce/category-menu/category-menu";
-import config from "~/config/dw";
 
 export interface levelZeroCategoriesQuery {
   categories: Array<CommerceSDK.Category>
 }
 
 export default component$(() => {
-  const apiResource = useResource$(async () => {
-    const response = await fetch(`${config.PUBLIC_API_ORIGIN}/api/commerce-sdk-react/category?id=${CAT_MENU_DEFAULT_ROOT_CATEGORY}&levels=${CAT_MENU_DEFAULT_NAV_SSR_DEPTH}`);
-    const data = await response.json();
-
-    return data.data as unknown as levelZeroCategoriesQuery;
-  });
-
   return (
-    <header class="px-3 lg:px-8 sticky top-0 z-10 bg-white shadow-lg text-black">
-      <div class="max-w-[75rem] mx-auto">
+    <header class="px-3 lg:px-8 sticky top-0 z-10 bg-white shadow-lg text-black flex items-center lg:min-h-[60px]">
+      <div class="max-w-[75rem] mx-auto w-full">
         <div class="lg:flex items-center">
-          <div class="flex items-center pt-3 lg:pt-0">
+          <div class="flex items-center pt-3 lg:pt-0 h-full">
             <LuMenu class="w-6 h-6 text-red-500 lg:hidden mr-4" />
             <Link href="/">
               <h2 class="text-[2rem] text-red-500">SYSTEMA</h2>
             </Link>
           </div>
 
-          <div class="hidden lg:block">
-            <Resource
-                value={apiResource}
-                onPending={() => <p>Loading...</p>}
-                onResolved={({ categories }) => (<CategoryMenu categories={categories} />)}
-                onRejected={(error) => <p>Error: {error.message}</p>}
-            />
-          </div>
+          <CategoryMenu />
 
-          <div class="py-3 lg:py-0">
+          <div class="py-3 lg:py-0 lg:ml-auto">
             <input
                 class="border border-red-500 focus-visible:border-red-500 rounded-lg h-10 w-60 w-full"
                 type="search"
